fafa00ff914eeb32c434dad748655b955ab39685
Craig Rose <craig.rose@digitalsense.com.au>
2015-05-13 16:05:49 +1000

    Update ISO for Debian 8

--- build-debian.sh
+++ build-debian.sh
@@ -3,8 +3,8 @@
 # Configurations
 BUILD_OSNAME="debian"
 BUILD_OSTYPE="Debian_64"
-BOX="debian-7.8.0-amd64"
-ISO_URL="http://mirror.overthewire.com.au/pub/debian-cd/7.8.0/amd64/iso-cd/debian-7.8.0-amd64-netinst.iso"
-ISO_MD5="a91fba5001cf0fbccb44a7ae38c63b6e"
+BOX="debian-8.0.0-amd64"
+ISO_URL="http://mirror.overthewire.com.au/pub/debian-cd/8.0.0/amd64/iso-cd/debian-8.0.0-amd64-netinst.iso"
+ISO_MD5="d9209f355449fe13db3963571b1f52d4"
 
 ./build.sh ${BUILD_OSNAME} ${BUILD_OSTYPE} ${BOX} ${ISO_URL} ${ISO_MD5}
